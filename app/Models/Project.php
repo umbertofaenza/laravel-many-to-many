c270486cede8d45f4f45cf76a2552e2a77feaa61
umbertofaenza <umberto.faenza@gmail.com>
2023-10-26 16:32:15 +0200

    add relationship to Technology into Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,4 +15,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
